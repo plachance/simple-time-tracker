c2a4f040d1eeb0c7b81ace704d839e0d8190cd9b
Patrice Lachance <patricelachance@gmail.com>
2016-10-18 20:33:58 -0400

    Add custom Intl service to localize dates from server side the same way as Twig Extension.

---
+++ src/AppBundle/Util/Intl.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Util;
+
+use DateTime;
+use IntlDateFormatter;
+use Symfony\Component\Translation\TranslatorInterface;
+
+/**
+ * Class Intl.
+ *
+ * @author Patrice Lachance <patricelachance@gmail.com>
+ */
+class Intl
+{
+	/**
+	 * @var TranslatorInterface
+	 */
+	protected $translator;
+
+	/**
+	 * @param TranslatorInterface $translator
+	 */
+	public function __construct(TranslatorInterface $translator)
+	{
+		$this->translator = $translator;
+	}
+
+	public function localizeDate(DateTime $date, $dateFormat = 'medium',
+		$timeFormat = 'medium', $locale = null, $format = null,
+		$calendar = 'gregorian')
+	{
+		$formatValues = [
+			'none' => IntlDateFormatter::NONE,
+			'short' => IntlDateFormatter::SHORT,
+			'medium' => IntlDateFormatter::MEDIUM,
+			'long' => IntlDateFormatter::LONG,
+			'full' => IntlDateFormatter::FULL,
+		];
+
+		$formatter = IntlDateFormatter::create(
+				$locale == null ? $this->translator->getLocale() : $locale,
+				$formatValues[$dateFormat], $formatValues[$timeFormat],
+				$date->getTimezone()->getName(),
+				'gregorian' === $calendar ? IntlDateFormatter::GREGORIAN : IntlDateFormatter::TRADITIONAL,
+				$format
+		);
+
+		return $formatter->format($date->getTimestamp());
+	}
+
+}
